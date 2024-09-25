efea90112394ade799edffb540d3a51253bc5519
jiangwei <1048851186@qq.com>
2024-09-25 10:50:21 +0800

    Initial Commit

---
+++ ImageQA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageQA
+//
+//  Created by edy on 2024/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
